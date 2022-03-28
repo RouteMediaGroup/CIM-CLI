dccb6067659dd46d6e484a92833dcaf23929319a
Michael Eckebrecht <direkt@route-media.info>
2022-03-28 23:06:56 +0200

    Change Alias

--- cim/app/mainctrl.php
+++ cim/app/mainctrl.php
@@ -1,15 +1,8 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of
+ * Description of MainCtrl
  *
- * @author MikeCorner BaseCtrl
+ * @author Michael Eckebrecht
  */
 
 declare(strict_types=1);
